764e0e4f8eeca9dabaf669100eb71f475ebcd5ef
ot-kim <kim.wangil@opentone.co.jp>
2021-04-08 13:41:30 +0000

    5.list rendering

--- main.js
+++ main.js
@@ -5,8 +5,14 @@ const app = Vue.createApp({
     data(){
         return {
             product: 'Socks',
-            image: '/assets/images/socks_green.jpg',
-            inStock: false
+            image: './assets/images/socks_green.jpg',
+            inStock: false,
+            details: ['50% cotton','30% wool','20% polyester'],
+            variants: [
+                { id:2234, color: 'green' },
+                { id:2235, color: 'blue' },
+                { id:2236, color: 'red' },
+            ]
         }
     }
 
