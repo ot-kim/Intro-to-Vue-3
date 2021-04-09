53cae6a0809669e595f80864fd884b23c78b96c6
ot-kim <kim.wangil@opentone.co.jp>
2021-04-09 00:14:29 +0000

    6. event handling

--- main.js
+++ main.js
@@ -4,15 +4,23 @@ const app = Vue.createApp({
     // Note : use arrow function 
     data(){
         return {
+            cart:0,
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
             inStock: false,
             details: ['50% cotton','30% wool','20% polyester'],
             variants: [
-                { id:2234, color: 'green' },
-                { id:2235, color: 'blue' },
-                { id:2236, color: 'red' },
+                { id:2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id:2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
+        }
+    },
+    method: {
+        addCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
 
