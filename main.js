d3daa83615f2333ff28f46bc44873bc56fbd3e30
ot-kim <kim.wangil@opentone.co.jp>
2021-04-09 01:14:23 +0000

    update

--- main.js
+++ main.js
@@ -1,30 +1,26 @@
+const product = "socks"
 const app = Vue.createApp({
-    data() {
+    // data: function(){}
+    // Note : use arrow function 
+    data(){
         return {
-            cart: 0,
+            cart:0,
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester'],
+            image: './assets/images/socks_green.jpg',
+            inStock: false,
+            details: ['50% cotton','30% wool','20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+                { id:2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id:2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
     },
-    methods: {
-        addToCart() {
+    method: {
+        addCart() {
             this.cart += 1
         },
-        // solution
-        removeFromCart() {
-            if (this.cart >= 1) {
-                this.cart -= 1
-            }
-        },
-        // solution
         updateImage(variantImage) {
             this.image = variantImage
-        }
+        },
     }
 })
