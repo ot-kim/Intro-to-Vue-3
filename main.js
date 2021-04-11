90087dd52074fe25eb4543f1f562f7c79050ae94
ot-kim <kim.wangil@opentone.co.jp>
2021-04-11 11:47:51 +0000

    add webpack

--- main.js
+++ main.js
@@ -1,35 +1,9 @@
-const product = "socks"
 const app = Vue.createApp({
-    // data: function(){}
-    // Note : use arrow function 
-    data(){
+    data() {
         return {
-            cart:0,
-            product: 'Socks',
-            brand: 'vue mastery',
-            selectedVariant: 0,
-            image: './assets/images/socks_green.jpg',
-            details: ['50% cotton','30% wool','20% polyester'],
-            variants: [
-                { id:2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-                { id:2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-            ]
+            cart: 0,
+            premium: true
         }
     },
-    method: {
-        addCart() {
-            this.cart += 1
-        },
-        updateVariant(index) {
-            this.selectedVariant = index
-        },
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product
-        },
-        image() {
-            return this.variants[this.selectedVariant].image
-        } 
-    }
+    methods: {}
 })
