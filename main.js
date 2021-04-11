88da292ae5f61e09966e3384d088e4686b6d6e05
ot-kim <kim.wangil@opentone.co.jp>
2021-04-11 10:33:43 +0000

    (8)computed properties

--- main.js
+++ main.js
@@ -6,12 +6,13 @@ const app = Vue.createApp({
         return {
             cart:0,
             product: 'Socks',
+            brand: 'vue mastery',
+            selectedVariant: 0,
             image: './assets/images/socks_green.jpg',
-            inStock: false,
             details: ['50% cotton','30% wool','20% polyester'],
             variants: [
-                { id:2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id:2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+                { id:2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id:2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
             ]
         }
     },
@@ -19,8 +20,16 @@ const app = Vue.createApp({
         addCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
         },
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        } 
     }
 })
